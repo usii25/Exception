b8406d1fa0ab81a759f3d1b4dae9cf565ea8354f
usii25 <karimatuzfirdausi@gmail.com>
2024-10-01 20:10:12 +0700

    Update Unchecked_Demo.java

--- Unchecked_Demo.java
+++ Unchecked_Demo.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Exception;
 
 public class Unchecked_Demo {
@@ -9,10 +5,11 @@ public class Unchecked_Demo {
     public static void main(String[] args) {
         try {
             int num[] = {1, 2, 3, 4};
-            System.out.println(num[4]);
+            System.out.println(num[4]); //mendeklarasi integer menggunakan array
+            //mencoba mengakses elemen array pada indeks ke-4 (`num[4]`) yang menyebabkan exception (out of bounds)
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Array melebihi batas :" + e);
+            //kemudian exception diambil oleh catch dan mencetak pesan "Array melebihi batas"
         }
-
     }
 }
