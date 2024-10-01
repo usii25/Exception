3e5be79dd69946c1e23f99609f94eecde8bcc2b6
usii25 <karimatuzfirdausi@gmail.com>
2024-10-01 22:31:13 +0700

    Update Finally2.java

--- Finally2.java
+++ Finally2.java
@@ -2,18 +2,15 @@ package Exception;
 
 Public class Finally2{
 	
-	Public class Finally2{
-    
-	  Public static void main(String args[]){
-		  int a[] = new[2];
+	Public static void main(String args[]){
+	  int a[] = new[2]; // membuat array berukuran 2
       try{
-	      System.out.println(“Access element three: “ + a[3]);
-      }catch(ArithmeticException e){
-	      System.out.println(“Exception thrown :” + e);
-      }finally{
-	      a[0] = 6;
-			  System.out.println(“First element value: ” + a[0]);
-			  System.out.println(“The finally statement is executed”);
-    }
+	      System.out.println(“Access element three: “ + a[3]); //mencoba mengakses elemen atau indeks ke 3 yang membuat exception (OutOfBounds)
+      }catch(ArithmeticException e){ //catch ini untuk menangani 'ArithmeticException'
+	      System.out.println(“Exception thrown :” + e); //pesan ini dicetak jika terjadi pengecualian
+      }finally{ // membuat finally statement
+	      a[0] = 6; // Array 'a' diindeks 0 diisi dengan nilai 6
+		  System.out.println(“First element value: ” + a[0]); // Mencetak nilai dari elemen pertama array, yaitu '6'
+		  System.out.println(“The finally statement is executed”); // Mencetak pesan bahwa blok 'finally' telah dieksekusi
   }
 }
