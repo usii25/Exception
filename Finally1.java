7b501dc529e5ef459d0e9fec41a6c058ece0b017
usii25 <karimatuzfirdausi@gmail.com>
2024-10-01 22:22:47 +0700

    Update Finally1.java

--- Finally1.java
+++ Finally1.java
@@ -2,20 +2,20 @@ package Exception;
 
 Public Finally1{
 	Public static void main(String args[]){
-		System.out.println(testFinallyBlock());
+		System.out.println(testFinallyBlock()); // memamnggil method testFinallyBlock
 	}
 }
 
 Private static int testFinallyBlock(){
-	Int a[] = new int[2];
+	Int a[] = new int[2]; // membuat array berukuran 2
   try{
-	  Return 1;
+	  Return 1; // mengembalikan nilai 1
   }catch(ArrayIndexOutOfBoundsException e) {	
-    System.out.println(“Exception thrown :” + e);
+    System.out.println(“Exception thrown :” + e); // Menangani pengecualian
   }finally {
-	  a[0] = 6;
-		System.out.println(“First element value: ” + a[0]);
-		System.out.println(“The finally statement is executed”);
+	  a[0] = 6; // Menetapkan nilai elemen pertama array menjadi 6
+		System.out.println(“First element value: ” + a[0]); // Mencetak nilai elemen pertama array
+		System.out.println(“The finally statement is executed”);  // Mencetak pesan dari blok finally
     }
-    Return 0;
+    Return 0; // Setelah finally, mengembalikan nilai 0
 }
