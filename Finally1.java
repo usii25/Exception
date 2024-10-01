38aa391c874bf1b49024395d2646017a3b8f16ad
usii25 <karimatuzfirdausi@gmail.com>
2024-10-01 21:48:14 +0700

    Create Finally1.java

---
+++ Finally1.java
@@ -0,0 +1,21 @@
+package Exception;
+
+Public Finally1{
+	Public static void main(String args[]){
+		System.out.println(testFinallyBlock());
+}
+
+Private static int testFinallyBlock(){
+	Int a[] = new int[2];
+  Try{
+	  Return 1;
+  }catch(ArrayIndexOutOfBoundsException e) {	
+    System.out.println(“Exception thrown :” + e);
+  }finally {
+	  a[0] = 6;
+		System.out.println(“First element value: ” + a[0]);
+		System.out.println(“The finally statement is executed”);
+    }
+    Return 0;
+  }
+}
