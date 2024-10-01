06d6293e686eefffeba8a636b0d910bd8506b3c4
usii25 <karimatuzfirdausi@gmail.com>
2024-10-01 20:42:44 +0700

    Update Unchecked_Demo.java

--- Unchecked_Demo.java
+++ Unchecked_Demo.java
@@ -4,8 +4,8 @@ public class Unchecked_Demo {
 
     public static void main(String[] args) {
         try {
-            int num[] = {1, 2, 3, 4};
-            System.out.println(num[4]); //mendeklarasi integer menggunakan array
+            int num[] = {1, 2, 3, 4}; //mendeklarasi integer menggunakan array
+            System.out.println(num[4]);
             //mencoba mengakses elemen array pada indeks ke-4 (`num[4]`) yang menyebabkan exception (out of bounds)
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Array melebihi batas :" + e);
