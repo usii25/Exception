844ddcbcef396885539eb11ab88b9afebc276e4b
usii25 <karimatuzfirdausi@gmail.com>
2024-10-02 13:03:16 +0700

    Update TugasException.java

--- TugasException.java
+++ TugasException.java
@@ -1,31 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Exception;
 
-import java.util.Scanner;
+import java.util.Scanner; //Mengimpor kelas Scanner dari java.util untuk mendapatkan input dari pengguna
 
 public class TugasException {
 
     public static void main(String[] args) throws ArithmeticException {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); //Membuat objek Scanner untuk membaca input dari pengguna
 
-        System.out.println("Masukan Nama Anda: ");
-        String a = input.nextLine();
-        char[] huruf = a.toCharArray();
-        for (char c : huruf) {
-            if (Character.isDigit(c)) {
-                throw new ArithmeticException("Format Nama Harus Menggunakan Huruf!");
+        System.out.println("Masukan Nama Anda: "); // Menampilkan prompt kepada pengguna untuk memasukkan nama
+        String a = input.nextLine(); //Membaca input nama pengguna dalam bentuk string
+        char[] huruf = a.toCharArray(); //Mengonversi string nama menjadi array karakter
+        for (char c : huruf) { //Menggunakan loop for-each untuk memeriksa setiap karakter dalam array 'huruf'
+            if (Character.isDigit(c)) { //Memeriksa apakah karakter tersebut adalah angka
+                throw new ArithmeticException("Format Nama Harus Menggunakan Huruf!"); //Jika ada karakter yang merupakan angka, lempar pengecualian 'ArithmeticException'
             }
         }
-        System.out.println("Masukan Umur Anda: ");
-        int b = input.nextInt();
-        if (b >= 0) {
-            System.out.println("Nama Anda: " + a);
-            System.out.println("Umur Anda: " + b);
-        } else {
-            throw new ArithmeticException("Format Umur Harus Menggunakan Angka!");
+        System.out.println("Masukan Umur Anda: "); //Menampilkan prompt kepada pengguna untuk memasukkan umur
+        int b = input.nextInt(); //Membaca input umur pengguna dalam bentuk integer
+        if (b >= 0) { //Memeriksa apakah umur yang dimasukkan tidak negatif
+            System.out.println("Nama Anda: " + a); // Menampilkan nama pengguna jika format nama sudah benar
+            System.out.println("Umur Anda: " + b); // Menampilkan nama pengguna jika format umur valid
+        } else { //Jika umur negatif atau tidak valid, jalankan blok ini
+            throw new ArithmeticException("Format Umur Harus Menggunakan Angka!"); // Lempar pengecualian 'ArithmeticException' jika umur yang dimasukkan negatif atau tidak valid
         }
     }
 }
